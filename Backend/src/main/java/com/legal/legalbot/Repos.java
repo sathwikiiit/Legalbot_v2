a87d1b500234c09f42e4d2c847afbf9ef7e1eacb
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2025-03-20 14:33:19 +0530

    Update Repos.java to use MySQL

--- Backend/src/main/java/com/legal/legalbot/Repos.java
+++ Backend/src/main/java/com/legal/legalbot/Repos.java
@@ -2,10 +2,9 @@ package com.legal.legalbot;
 
 
 import java.util.List;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface Repos extends MongoRepository<Suit,Long>{
-    List<Suit> findByUser(@Param("user")String user);
+public interface Repos extends JpaRepository<Suit,Long>{
+    List<Suit> findByUser(@Param("created_by")String user);
  }
