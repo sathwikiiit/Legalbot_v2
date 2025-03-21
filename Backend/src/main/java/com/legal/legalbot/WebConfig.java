b32047a33eff36e8737bf0195f34bcf3a5a3bab8
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2025-03-21 08:37:48 +0000

    Corrected origins

--- Backend/src/main/java/com/legal/legalbot/WebConfig.java
+++ Backend/src/main/java/com/legal/legalbot/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("*") // Allow for the specific "/auth" endpoint
-            .allowedOrigins("127.0.0.1:4200") // Allow requests from your Angular app origin
+            .allowedOrigins("http://localhost:4200", "https://legalbot-v2.vercel.app") // Corrected origins
             .allowedMethods("GET", "POST") // Allow specific HTTP methods (adjust as needed)
             .allowedHeaders("*"); // Allow all headers (adjust if necessary for security)
     }
