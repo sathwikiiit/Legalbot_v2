4729f20b1ca0c5aa8eda5bd9234fcdf76ccc36bc
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2025-03-21 05:37:59 +0000

    update small error

--- Backend/src/main/java/com/legal/legalbot/LegalbotApplication.java
+++ Backend/src/main/java/com/legal/legalbot/LegalbotApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = "@CrossOrigin(origins = 'https://legalbot-v2.vercel.app')")
+@CrossOrigin(origins = "https://legalbot-v2.vercel.app")
 public class LegalbotApplication {
 
 	public static void main(String[] args) {
