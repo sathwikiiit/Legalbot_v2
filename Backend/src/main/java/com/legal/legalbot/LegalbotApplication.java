9967f9fca44d252c305fa2713d6e5799c86bb2c8
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2025-03-25 05:10:01 +0000

    Remove multiple CORS

--- Backend/src/main/java/com/legal/legalbot/LegalbotApplication.java
+++ Backend/src/main/java/com/legal/legalbot/LegalbotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = {"https://legalbot-v2.vercel.app", "http://localhost:4200"})
 public class LegalbotApplication {
  
 	public static void main(String[] args) {
