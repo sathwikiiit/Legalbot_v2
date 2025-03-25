ac5117d1c5699ea2dd41151e7e10c720db6c2975
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2025-03-25 05:57:01 +0000

    modified:   Backend/.gitignore 	modified:   Backend/src/main/java/com/legal/legalbot/WebConfig.java

--- Backend/src/main/java/com/legal/legalbot/WebConfig.java
+++ Backend/src/main/java/com/legal/legalbot/WebConfig.java
@@ -3,6 +3,7 @@ package com.legal.legalbot;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedOrigins("http://localhost:4200", "https://legalbot-v2.vercel.app") // Corrected origins
             .allowedMethods("GET", "POST","OPTIONS") // Allow specific HTTP methods (adjust as needed)
             .allowedHeaders("*"); // Allow all headers (adjust if necessary for security)
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/browser/", "classpath:/static/");
     }
 }
