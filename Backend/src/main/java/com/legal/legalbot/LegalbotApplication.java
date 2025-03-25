aa5d8998a52ebe8d52c2717cc66385e43cef9149
sathwikiiit <71497898+sathwikiiit@users.noreply.github.com>
2025-03-25 05:09:46 +0000

    update

--- Backend/src/main/java/com/legal/legalbot/LegalbotApplication.java
+++ Backend/src/main/java/com/legal/legalbot/LegalbotApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 @CrossOrigin(origins = {"https://legalbot-v2.vercel.app", "http://localhost:4200"})
 public class LegalbotApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(LegalbotApplication.class, args);
 	}
